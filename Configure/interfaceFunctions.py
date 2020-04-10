4d6214aa3fa364b3191c2005033addc2f82115b2
theresaphan2000 <t.phan4@students.clark.edu>
2020-04-10 14:14:15 -0700

    does the same thing, just rewrote into classes for frames

--- Configure/interfaceFunctions.py
+++ Configure/interfaceFunctions.py
@@ -5,7 +5,6 @@ import tkinter as tk
 from tkinter import filedialog, Text
 import os
 
-
 # --------------------------------------------------------------------------------------------------
 
 # firmware folder name(subjected to change)
@@ -17,12 +16,37 @@ qmkdir = ''
 
 # root used in interfaceVisuals.py
 root = tk.Tk()  # needed " think of it as a base'
+root.title("hello")
+root.geometry("500x200")
+root.configure(background='#21292d')
 
 
 # Function that stores user input for keymapping
+# os system to change directory to qmk_firmware folder etc
+def makeHex(path):
+    # keyboardpath = path + '\\keyboards\\Tmacro
+    subprocess.run('ls', shell=True,
+                   cwd=path)  # list directory, use shell, change current working directory to path give
+    # run vs open vs check_call??
+    subprocess.run('make Tmacro:default', shell=True)  # make hexfile
+    return
+
+
+# function to check and allow user to select directory
+
+def getDirectory(dirFrame):
+    global qmkdir
+    qmkdir = filedialog.askdirectory(parent=root, initialdir="/", title="Select your qmk_firmware directory: ")
+    while os.path.basename(qmkdir) != firmware and qmkdir != '':  # if doesnt return empty string
+        qmkdir = filedialog.askdirectory(mustexist=True, title="Select your qmk_firmware directory: ")
+    dirLabel = tk.Label(dirFrame, pady=.1, text=qmkdir, bg="#52606D", fg="white", font="Helvetica 8").place(relx=0,
+                                                                                                            rely=0)
+    return qmkdir
+
+
 # Generic Button Class
 class buttonAttributes(tk.Button):
-    def __init__(self, frameRef, rx, ry, **kwargs):
+    def __init__(self, frameRef,  rx, ry,widgetFrame,  **kwargs):
         tk.Button.__init__(self, master=frameRef, **kwargs)
 
         self.config(padx=1, relief="solid", pady=1, fg="#97a4aa", bd=0, highlightthickness=0)
@@ -37,4 +61,15 @@ class buttonAttributes(tk.Button):
         self['bg'] = "#293439"
 
 
+#######################Page Classes#####################################
+class pageOne(tk.Frame):
+    def __init__(self, frameRoot, **kwargs):
+        tk.Frame.__init__(self, root, **kwargs)
+        self.place(relwidth=.8, relheight=.7, relx=.1, rely=.1)
+        dirFrame = tk.LabelFrame(frameRoot, bg="#52606D", relief="sunken", bd=1)
+        dirFrame.place(relwidth=.5, relheight=.1, relx=.3, rely=.26)
 
+        openDir = buttonAttributes(self, .05, .2, dirFrame, text="Show Folder", bg="#293439", command=lambda: getDirectory(dirFrame))
+
+class pageTow(tk.Frame):
+    def __init__
--- Configure/interfaceVisuals.py
+++ Configure/interfaceVisuals.py
@@ -1,54 +1,28 @@
 #!/usr/bin/env python3
 from interfaceFunctions import *
-# os system to change directory to qmk_firmware folder etc
-def makeHex(path):
-    # keyboardpath = path + '\\keyboards\\Tmacro
-    subprocess.run('ls', shell=True,
-                   cwd=path)  # list directory, use shell, change current working directory to path give
-    # run vs open vs check_call??
-    subprocess.run('make Tmacro:default', shell=True)  # make hexfile
-    return
 
+##################################################GUI DISPLAY FRAMES ##############################################################################
 
-# function to check and allow user to select directory
-
-def getDirectory(dirFrame):
-    global qmkdir
-    qmkdir = filedialog.askdirectory(parent=root, initialdir="/", title="Select your qmk_firmware directory: ")
-    while os.path.basename(qmkdir) != firmware and qmkdir != '':  # if doesnt return empty string
-        qmkdir = filedialog.askdirectory(mustexist=True, title="Select your qmk_firmware directory: ")
-    dirLabel = tk.Label(dirFrame, pady=.1, text=qmkdir, bg="#52606D", fg="white", font="Helvetica 8").place(relx=0, rely=0)  # FIX ISSUE
-
-    return qmkdir
-# def nextButtonFunction():
-
-def dirScreen():
-    root.title("hello")
-    root.geometry("500x200")
-    root.configure(background='#21292d')
-
-    # FRAME
-    frame = tk.Frame(root, bg="#293439")
-    frame.config(relief="solid", highlightthickness=1, highlightbackground="#97a4aa")
-    frame.place(relwidth=.8, relheight=.7, relx=.1, rely=.1)
-    # Border Label window
-    dirFrame = tk.LabelFrame(root, bg="#52606D", relief="sunken", bd=1)
-    dirFrame.place(relwidth=.5, relheight=.1, relx=.3, rely=.26)
-
-    openDir = buttonAttributes(frame, .05, .2, text="Show Folder", bg="#293439", command= lambda: getDirectory(dirFrame))
-
-
-
-    #nextButton = buttonAttributes(frame, .65, .6, text="Next", bg="#293439", command=nextButtonFunction)
-    root.update()
-
-    root.mainloop()
-
-
+# def dirScreen():
+#     global root
+#     # FRAME
+#     frame = tk.Frame()
+#     frame.config(relief="solid", highlightthickness=1, highlightbackground="#97a4aa")
+#     frame.place(relwidth=.8, relheight=.7, relx=.1, rely=.1)
+#     # Border Label window
+#     dirFrame = tk.LabelFrame(root, bg="#52606D", relief="sunken", bd=1)
+#     dirFrame.place(relwidth=.5, relheight=.1, relx=.3, rely=.26)
+#
+#     openDir = buttonAttributes(frame, .05, .2, text="Show Folder", bg="#293439", command=lambda: getDirectory(dirFrame))
+#
+#     # nextButton = buttonAttributes(frame, .65, .6, text="Next", bg="#293439", command=nextButtonFunction)
+#     root.mainloop()
 
 
 def main():
-    dirScreen()
+    global root
+    dirScreen = pageOne(root, bg="#293439", relief="solid", highlightthickness=1,highlightbackground="#97a4aa")
 
+    root.mainloop()
 
 main()
