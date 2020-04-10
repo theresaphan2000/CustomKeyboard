8916ad170a54faaba28b6cabef6addd88b8e247d
theresaphan2000 <t.phan4@students.clark.edu>
2020-04-10 12:12:18 -0700

    issue with printing directory label, Need fix

--- Configure/interfaceFunctions.py
+++ Configure/interfaceFunctions.py
@@ -4,39 +4,54 @@ from tkinter import *
 import tkinter as tk
 from tkinter import filedialog, Text
 import os
-#--------------------------------------------------------------------------------------------------
 
-#firmware folder name(subjected to change)
+# --------------------------------------------------------------------------------------------------
+
+# firmware folder name(subjected to change)
 firmware = 'qmk_firmware'
-#Global variable for path
-ULTIMATEPATH = 'hello'
+# Global variable for path
+qmkdir = ''
 
 # Global array for keymapping
 
-#root used in interfaceVisuals.py
+# root used in interfaceVisuals.py
 root = tk.Tk()  # needed " think of it as a base'
 
-# Function that stores user input for keymapping
-# def getKeys():
 
-#os system to change directory to qmk_firmware folder etc
+# Function that stores user input for keymapping
+# Generic Button Class
+class buttonAttributes(tk.Button):
+    def __init__(self, frameRef, rx, ry, **kwargs):
+        tk.Button.__init__(self, master=frameRef, **kwargs)
+
+        self.config(padx=1, relief="solid", pady=1, fg="#97a4aa", bd=0, highlightthickness=0)
+        self.place(relwidth=.2, relheight=.2, relx=rx, rely=ry)
+        self.bind("<Enter>", self.on_enter)
+        self.bind("<Leave>", self.on_leave)
+
+    def on_enter(self, e):
+        self['bg'] = "black"
+
+    def on_leave(self, e):
+        self['bg'] = "#293439"
+
+
+# os system to change directory to qmk_firmware folder etc
 def makeHex(path):
     # keyboardpath = path + '\\keyboards\\Tmacro
-    subprocess.run('ls', shell=True, cwd=path) #list directory, use shell, change current working directory to path give
-    #run vs open vs check_call??
-    subprocess.run('make Tmacro:default', shell=True) #make hexfile
+    subprocess.run('ls', shell=True,
+                   cwd=path)  # list directory, use shell, change current working directory to path give
+    # run vs open vs check_call??
+    subprocess.run('make Tmacro:default', shell=True)  # make hexfile
     return
+
+
 # function to check and allow user to select directory
 
 def getDirectory():
+    global qmkdir
     qmkdir = filedialog.askdirectory(parent=root, initialdir="/", title="Select your qmk_firmware directory: ")
-    while os.path.basename(qmkdir) != firmware and qmkdir != '': #if doesnt return empty string
-          qmkdir = filedialog.askdirectory(mustexist=True, title="Select your qmk_firmware directory: ")
+    while os.path.basename(qmkdir) != firmware and qmkdir != '':  # if doesnt return empty string
+        qmkdir = filedialog.askdirectory(mustexist=True, title="Select your qmk_firmware directory: ")
 
-    global ULTIMATEPATH
-    ULTIMATEPATH = qmkdir
-    dirBox = tk.Label(root, bg="black", fg="#97a4aa", text=ULTIMATEPATH, borderwidth=1.2, relief="sunken", font="Helvetica 11")#.grid(row=0, column=1 )
-
-    dirBox.pack()
-    return
-
+    return qmkdir
--- Configure/interfaceVisuals.py
+++ Configure/interfaceVisuals.py
@@ -1,44 +1,27 @@
 #!/usr/bin/env python3
 from interfaceFunctions import *
-#
-class buttonAttributes(tk.Button):
-    def __init__(self,  frameRef,rx ,ry ,**kwargs):
-
-        tk.Button.__init__(self, master=frameRef, **kwargs)
-
-        self.config(padx=1, relief="solid", pady=1, fg="#97a4aa", bd=0, highlightthickness=0)
-        self.place(relwidth=.2, relheight=.2, relx=rx, rely=ry)
-        self.bind("<Enter>", self.on_enter)
-        self.bind("<Leave>", self.on_leave)
-
-    def on_enter(self, e):
-        self['bg']= "black"
-
-    def on_leave(self,e):
-        self['bg'] = "#293439"
 
 
-
-
-def main():
+def dirScreen():
     root.title("hello")
     root.geometry("500x200")
     root.configure(background='#21292d')
-# # CANVAS
-#     canvas = tk.Canvas(root, height=200, width=500, bg="pink")
-#     canvas.grid(row=0, column=1)  # needed fill, exapnd, a geometry manager
-#     canvas.update()
 
-#FRAME
+    # FRAME
     frame = tk.Frame(root, bg="#293439")
-    frame.config(relief="solid", highlightthickness=1,highlightbackground="#97a4aa" )
+    frame.config(relief="solid", highlightthickness=1, highlightbackground="#97a4aa")
     frame.place(relwidth=.8, relheight=.7, relx=.1, rely=.1)
     # Border Label window
+    dirFrame = tk.LabelFrame(root, bg="#52606D", relief="sunken", bd=1)
+    dirFrame.place(relwidth=.5, relheight=.1, relx=.3, rely=.26)
 
-    openDir = buttonAttributes(frame, .05, .2,  text="Show Folder", command=getDirectory)
-    newDir = buttonAttributes( frame, .5, .5, text="Show Folder", command=getDirectory)
+    openDir = buttonAttributes(frame, .05, .2, text="Show Folder", bg="#293439", command=getDirectory)
+    nextButton = buttonAttributes(frame, .05, .5, text="Next", bg="#293439", command=getDirectory)
+    dirLabel = tk.Label(dirFrame, pady=.1, text=qmkdir, bg="#52606D", fg="white", font="Helvetica 8").place(relx=0,rely=0)
+    print(qmkdir)
+    root.update()
 
     root.mainloop()
 
 
-main()
+dirScreen()
