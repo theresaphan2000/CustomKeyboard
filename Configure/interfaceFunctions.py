4118570ad185465cd1767d1db5aca0c40cca68e6
theresaphan2000 <t.phan4@students.clark.edu>
2020-04-10 12:44:01 -0700

    fixed printing issue. add more feagutres to drop down bar

--- Configure/interfaceFunctions.py
+++ Configure/interfaceFunctions.py
@@ -5,6 +5,7 @@ import tkinter as tk
 from tkinter import filedialog, Text
 import os
 
+
 # --------------------------------------------------------------------------------------------------
 
 # firmware folder name(subjected to change)
@@ -36,22 +37,4 @@ class buttonAttributes(tk.Button):
         self['bg'] = "#293439"
 
 
-# os system to change directory to qmk_firmware folder etc
-def makeHex(path):
-    # keyboardpath = path + '\\keyboards\\Tmacro
-    subprocess.run('ls', shell=True,
-                   cwd=path)  # list directory, use shell, change current working directory to path give
-    # run vs open vs check_call??
-    subprocess.run('make Tmacro:default', shell=True)  # make hexfile
-    return
 
-
-# function to check and allow user to select directory
-
-def getDirectory():
-    global qmkdir
-    qmkdir = filedialog.askdirectory(parent=root, initialdir="/", title="Select your qmk_firmware directory: ")
-    while os.path.basename(qmkdir) != firmware and qmkdir != '':  # if doesnt return empty string
-        qmkdir = filedialog.askdirectory(mustexist=True, title="Select your qmk_firmware directory: ")
-
-    return qmkdir
--- Configure/interfaceVisuals.py
+++ Configure/interfaceVisuals.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
 from interfaceFunctions import *
+# os system to change directory to qmk_firmware folder etc
+def makeHex(path):
+    # keyboardpath = path + '\\keyboards\\Tmacro
+    subprocess.run('ls', shell=True,
+                   cwd=path)  # list directory, use shell, change current working directory to path give
+    # run vs open vs check_call??
+    subprocess.run('make Tmacro:default', shell=True)  # make hexfile
+    return
 
+
+# function to check and allow user to select directory
+
+def getDirectory(dirFrame):
+    global qmkdir
+    qmkdir = filedialog.askdirectory(parent=root, initialdir="/", title="Select your qmk_firmware directory: ")
+    while os.path.basename(qmkdir) != firmware and qmkdir != '':  # if doesnt return empty string
+        qmkdir = filedialog.askdirectory(mustexist=True, title="Select your qmk_firmware directory: ")
+    dirLabel = tk.Label(dirFrame, pady=.1, text=qmkdir, bg="#52606D", fg="white", font="Helvetica 8").place(relx=0, rely=0)  # FIX ISSUE
+
+    return qmkdir
+# def nextButtonFunction():
 
 def dirScreen():
     root.title("hello")
@@ -15,13 +35,20 @@ def dirScreen():
     dirFrame = tk.LabelFrame(root, bg="#52606D", relief="sunken", bd=1)
     dirFrame.place(relwidth=.5, relheight=.1, relx=.3, rely=.26)
 
-    openDir = buttonAttributes(frame, .05, .2, text="Show Folder", bg="#293439", command=getDirectory)
-    nextButton = buttonAttributes(frame, .05, .5, text="Next", bg="#293439", command=getDirectory)
-    dirLabel = tk.Label(dirFrame, pady=.1, text=qmkdir, bg="#52606D", fg="white", font="Helvetica 8").place(relx=0,rely=0)
-    print(qmkdir)
+    openDir = buttonAttributes(frame, .05, .2, text="Show Folder", bg="#293439", command= lambda: getDirectory(dirFrame))
+
+
+
+    #nextButton = buttonAttributes(frame, .65, .6, text="Next", bg="#293439", command=nextButtonFunction)
     root.update()
 
     root.mainloop()
 
 
-dirScreen()
+
+
+def main():
+    dirScreen()
+
+
+main()
